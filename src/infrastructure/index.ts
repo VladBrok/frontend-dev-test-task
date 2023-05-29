979896fe57214555065738f51227529560b35a02
VladBrok <vlad.brok99@gmail.com>
2023-05-29 12:13:42 +0300

    remove axios because api will be mock

--- src/infrastructure/index.ts
+++ src/infrastructure/index.ts
@@ -1,47 +1,19 @@
-import axios, { AxiosRequestConfig, AxiosResponse } from "axios"
-import { delay } from "../lib/delay"
-
-const axiosInstance = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL,
-})
-
-const ARTIFICIAL_DELAY_MS = 500
-
-axiosInstance.interceptors.request.use(async (config) => {
-  await delay(ARTIFICIAL_DELAY_MS)
-  return config
-})
-
+// TODO
 class InfrastructureClient {
-  async get<T>(
-    url: string,
-    config?: AxiosRequestConfig,
-  ): Promise<AxiosResponse<T>> {
-    const response = await axiosInstance.get<T>(url, config)
-    return response
+  async get<T>(url: string): Promise<T> {
+    return JSON.parse(localStorage.getItem(url) || "{}")
   }
 
-  async post<D, T>(
-    url: string,
-    data: D,
-    config?: AxiosRequestConfig,
-  ): Promise<AxiosResponse<T>> {
-    const response = await axiosInstance.post<T>(url, data, config)
-    return response
+  async post<D, T>(url: string, data: D): Promise<T> {
+    return JSON.parse(localStorage.getItem(url) || "{}")
   }
 
-  async put<D, T>(
-    url: string,
-    data: D,
-    config?: AxiosRequestConfig,
-  ): Promise<AxiosResponse<T>> {
-    const response = await axiosInstance.put<T>(url, data, config)
-    return response
+  async put<D, T>(url: string, data: D): Promise<T> {
+    return JSON.parse(localStorage.getItem(url) || "{}")
   }
 
-  async delete<T>(url: string, config?: AxiosRequestConfig) {
-    const response = await axiosInstance.delete<T>(url, config)
-    return response
+  async delete<T>(url: string) {
+    return JSON.parse(localStorage.getItem(url) || "{}")
   }
 }
 
