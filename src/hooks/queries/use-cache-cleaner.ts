fcd0f919187eb0db9d5f3ec4ab5ef4f08b6f4e4a
VladBrok <vlad.brok99@gmail.com>
2023-06-01 11:38:24 +0300

    add loading and error states to the booking form, create a cache cleaner hook

---
+++ src/hooks/queries/use-cache-cleaner.ts
@@ -0,0 +1,19 @@
+import { useQueryClient } from "@tanstack/react-query"
+import { useEffect, useRef } from "react"
+
+export default function (queryKeys: string[]) {
+  const queryClient = useQueryClient()
+  const isCleaned = useRef(false)
+
+  useEffect(() => {
+    if (isCleaned.current) {
+      return
+    }
+
+    isCleaned.current = true
+
+    queryKeys.forEach((key) => {
+      queryClient.removeQueries([key])
+    })
+  }, [queryClient, queryKeys])
+}
--- src/routes/booking.tsx
+++ src/routes/booking.tsx
@@ -2,6 +2,7 @@ import { Formik } from "formik"
 import Button from "react-bootstrap/Button"
 import Container from "react-bootstrap/Container"
 import Form from "react-bootstrap/Form"
+import Spinner from "react-bootstrap/Spinner"
 import useTablesQuery from "../hooks/queries/use-tables-query"
 import getUnavailableDates from "../lib/get-unavailable-dates"
 import useBookingsQuery from "../hooks/queries/use-bookings-query"
@@ -32,6 +33,7 @@ import { assert } from "../lib/assert"
 import blockDatepickerManualEditing from "../lib/block-datepicker-manual-editing"
 import scrollToFirstAvailableTime from "../lib/scroll-to-first-available-time"
 import pluralizeHours from "../lib/pluralize/pluralize-hours"
+import useCacheCleaner from "../hooks/queries/use-cache-cleaner"
 
 const Alert = lazy(() => import("react-bootstrap/Alert"))
 const AlertHeading = lazy(() =>
@@ -42,8 +44,6 @@ const AlertHeading = lazy(() =>
 
 export default function Booking() {
   const user = useCurrentUser()
-  const tablesQuery = useTablesQuery()
-  const bookingsQuery = useBookingsQuery()
   const dispatch = useDispatch()
   const navigate = useNavigate()
   const [isSubmitClicked, setIsSubmitClicked] = useState(false)
@@ -52,6 +52,10 @@ export default function Booking() {
   const queryClient = useQueryClient()
   const submitButtonRef = useRef<HTMLButtonElement | null>(null)
   const timeContainerRef = useRef<HTMLDivElement | null>(null)
+
+  const tablesQuery = useTablesQuery()
+  const bookingsQuery = useBookingsQuery()
+  useCacheCleaner([QUERY_KEYS.BOOKINGS, QUERY_KEYS.TABLES])
 
   const unavailableDates = useMemo(() => {
     if (!bookingsQuery.data) {
@@ -126,7 +130,11 @@ export default function Booking() {
     submitButtonRef.current?.click()
   }, [bookingRequest, isRevalidating])
 
-  // TODO: queries loading / error
+  const spinner = (
+    <div className="d-flex justify-content-center">
+      <Spinner animation="grow" />
+    </div>
+  )
 
   if (!user) {
     return <></>
@@ -134,152 +142,190 @@ export default function Booking() {
 
   return (
     <Container>
-      {canBook ? (
-        <h1 className="fs-2 text-center mb-5">Забронировать</h1>
-      ) : (
+      <Suspense fallback={spinner}>
+        {(tablesQuery.isLoading || bookingsQuery.isLoading) && spinner}
+
+        {tablesQuery.isError && (
+          <Alert variant="danger" className="w-50 mx-auto">
+            Не удалось загрузить данные о доступных столиках. Попробуйте
+            перезагрузить страницу.
+          </Alert>
+        )}
+
+        {bookingsQuery.isError && (
+          <Alert variant="danger" className="w-50 mx-auto">
+            Не удалось загрузить данные о бронированиях. Попробуйте
+            перезагрузить страницу.
+          </Alert>
+        )}
+
+        {tablesQuery.isSuccess && bookingsQuery.isSuccess && (
+          <>
+            {canBook ? (
+              <h1 className="fs-2 text-center mb-5">Забронировать</h1>
+            ) : (
+              <Suspense>
+                <Alert variant="primary" className="w-50 mx-auto">
+                  <AlertHeading>Бронирование недоступно</AlertHeading>
+                  <p className="mb-0">
+                    Все столики уже забронированы. Возвращайтесь позже
+                  </p>
+                </Alert>
+              </Suspense>
+            )}
+
+            <Formik
+              validationSchema={getBookingSchema(
+                unavailableDates,
+                unavailableTimes,
+                availableGuestCount,
+              )}
+              onSubmit={bookingRequest.mutate}
+              initialValues={
+                {
+                  date: null,
+                  time: null,
+                  guestCount: "",
+                } as any
+              }
+              validateOnChange={isSubmitClicked}
+            >
+              {({
+                handleSubmit,
+                handleChange,
+                setFieldValue,
+                values,
+                errors,
+              }) => (
+                <Form
+                  noValidate
+                  onSubmit={handleSubmit}
+                  className="mx-auto"
+                  style={{ width: "max(285px, 27%)" }}
+                >
+                  <fieldset disabled={!canBook}>
+                    <Form.Group
+                      controlId="validationFormikDate"
+                      className="mt-3"
+                    >
+                      <Form.Label>Дата</Form.Label>
+                      <DatePicker
+                        customInput={
+                          <Form.Control
+                            type="text"
+                            disabled
+                            readOnly
+                            isInvalid={!!errors.date}
+                          />
+                        }
+                        dateFormat="dd.MM.yyyy"
+                        placeholderText="Дата бронирования"
+                        name="date"
+                        selected={values.date}
+                        onChange={(val) => {
+                          setSelectedDate(val)
+                          setFieldValue("date", val)
+                        }}
+                        onFocus={blockDatepickerManualEditing}
+                        minDate={getToday()}
+                        excludeDates={unavailableDates}
+                      />
+                      <Form.Control.Feedback type="invalid">
+                        <>{errors.date}</>
+                      </Form.Control.Feedback>
+                    </Form.Group>
+                    <Form.Group
+                      controlId="validationFormikTime"
+                      className="mt-3"
+                    >
+                      <Form.Label>Время</Form.Label>
+                      <DatePicker
+                        customInput={
+                          <Form.Control
+                            type="text"
+                            disabled
+                            readOnly
+                            isInvalid={!!errors.time}
+                          />
+                        }
+                        calendarContainer={({ children, className }) => (
+                          <div className={className} ref={timeContainerRef}>
+                            {children}
+                          </div>
+                        )}
+                        showTimeSelect
+                        showTimeSelectOnly
+                        timeCaption=""
+                        timeIntervals={BOOKING_DURATION_HOURS * 60}
+                        dateFormat="HH:mm"
+                        placeholderText="Время бронирования"
+                        name="time"
+                        selected={values.time}
+                        onChange={(val) => {
+                          setFieldValue("time", val)
+                        }}
+                        onFocus={blockDatepickerManualEditing}
+                        onCalendarOpen={() =>
+                          !values.time &&
+                          scrollToFirstAvailableTime(timeContainerRef)
+                        }
+                        excludeTimes={unavailableTimes}
+                        minTime={BOOKING_START_MIN_TIME}
+                        maxTime={BOOKING_START_MAX_TIME}
+                      />
+                      <Form.Control.Feedback type="invalid">
+                        <>{errors.time}</>
+                      </Form.Control.Feedback>
+                      <Form.Text className="text-muted">
+                        Продолжительность бронирования:{" "}
+                        {pluralizeHours(BOOKING_DURATION_HOURS)}
+                      </Form.Text>
+                    </Form.Group>
+                    <Form.Group
+                      controlId="validationFormikGuests"
+                      className="mt-3"
+                    >
+                      <Form.Label>Гости</Form.Label>
+                      <Form.Control
+                        type="number"
+                        placeholder="Количество гостей"
+                        name="guestCount"
+                        value={values.guestCount}
+                        onChange={handleChange}
+                        isInvalid={!!errors.guestCount}
+                      />
+                      <Form.Control.Feedback type="invalid">
+                        {errors.guestCount}
+                      </Form.Control.Feedback>
+                    </Form.Group>
+                    <Button
+                      ref={submitButtonRef}
+                      type="submit"
+                      variant="dark"
+                      onClick={() => setIsSubmitClicked(true)}
+                      disabled={bookingRequest.isLoading}
+                      className="mt-4 w-100 d-block"
+                    >
+                      {submitButtonText}
+                    </Button>
+                  </fieldset>
+                </Form>
+              )}
+            </Formik>
+          </>
+        )}
+
         <Suspense>
-          <Alert variant="primary" className="w-50 mx-auto">
-            <AlertHeading>Бронирование недоступно</AlertHeading>
-            <p className="mb-0">
-              Все столики уже забронированы. Возвращайтесь позже
-            </p>
-          </Alert>
+          {bookingRequest.isError && (
+            <Alert
+              variant="danger"
+              style={{ maxWidth: "50%" }}
+              className="mx-auto mt-4"
+            >
+              {errorText}
+            </Alert>
+          )}
         </Suspense>
-      )}
-
-      <Formik
-        validationSchema={getBookingSchema(
-          unavailableDates,
-          unavailableTimes,
-          availableGuestCount,
-        )}
-        onSubmit={bookingRequest.mutate}
-        initialValues={
-          {
-            date: null,
-            time: null,
-            guestCount: "",
-          } as any
-        }
-        validateOnChange={isSubmitClicked}
-      >
-        {({ handleSubmit, handleChange, setFieldValue, values, errors }) => (
-          <Form
-            noValidate
-            onSubmit={handleSubmit}
-            className="mx-auto"
-            style={{ width: "max(285px, 27%)" }}
-          >
-            <fieldset disabled={!canBook}>
-              <Form.Group controlId="validationFormikDate" className="mt-3">
-                <Form.Label>Дата</Form.Label>
-                <DatePicker
-                  customInput={
-                    <Form.Control
-                      type="text"
-                      disabled
-                      readOnly
-                      isInvalid={!!errors.date}
-                    />
-                  }
-                  dateFormat="dd.MM.yyyy"
-                  placeholderText="Дата бронирования"
-                  name="date"
-                  selected={values.date}
-                  onChange={(val) => {
-                    setSelectedDate(val)
-                    setFieldValue("date", val)
-                  }}
-                  onFocus={blockDatepickerManualEditing}
-                  minDate={getToday()}
-                  excludeDates={unavailableDates}
-                />
-                <Form.Control.Feedback type="invalid">
-                  <>{errors.date}</>
-                </Form.Control.Feedback>
-              </Form.Group>
-              <Form.Group controlId="validationFormikTime" className="mt-3">
-                <Form.Label>Время</Form.Label>
-                <DatePicker
-                  customInput={
-                    <Form.Control
-                      type="text"
-                      disabled
-                      readOnly
-                      isInvalid={!!errors.time}
-                    />
-                  }
-                  calendarContainer={({ children, className }) => (
-                    <div className={className} ref={timeContainerRef}>
-                      {children}
-                    </div>
-                  )}
-                  showTimeSelect
-                  showTimeSelectOnly
-                  timeCaption=""
-                  timeIntervals={BOOKING_DURATION_HOURS * 60}
-                  dateFormat="HH:mm"
-                  placeholderText="Время бронирования"
-                  name="time"
-                  selected={values.time}
-                  onChange={(val) => {
-                    setFieldValue("time", val)
-                  }}
-                  onFocus={blockDatepickerManualEditing}
-                  onCalendarOpen={() =>
-                    !values.time && scrollToFirstAvailableTime(timeContainerRef)
-                  }
-                  excludeTimes={unavailableTimes}
-                  minTime={BOOKING_START_MIN_TIME}
-                  maxTime={BOOKING_START_MAX_TIME}
-                />
-                <Form.Control.Feedback type="invalid">
-                  <>{errors.time}</>
-                </Form.Control.Feedback>
-                <Form.Text className="text-muted">
-                  Продолжительность бронирования:{" "}
-                  {pluralizeHours(BOOKING_DURATION_HOURS)}
-                </Form.Text>
-              </Form.Group>
-              <Form.Group controlId="validationFormikGuests" className="mt-3">
-                <Form.Label>Гости</Form.Label>
-                <Form.Control
-                  type="number"
-                  placeholder="Количество гостей"
-                  name="guestCount"
-                  value={values.guestCount}
-                  onChange={handleChange}
-                  isInvalid={!!errors.guestCount}
-                />
-                <Form.Control.Feedback type="invalid">
-                  {errors.guestCount}
-                </Form.Control.Feedback>
-              </Form.Group>
-              <Button
-                ref={submitButtonRef}
-                type="submit"
-                variant="dark"
-                onClick={() => setIsSubmitClicked(true)}
-                disabled={bookingRequest.isLoading}
-                className="mt-4 w-100 d-block"
-              >
-                {submitButtonText}
-              </Button>
-            </fieldset>
-          </Form>
-        )}
-      </Formik>
-
-      <Suspense>
-        {bookingRequest.isError && (
-          <Alert
-            variant="danger"
-            style={{ maxWidth: "50%" }}
-            className="mx-auto mt-4"
-          >
-            {errorText}
-          </Alert>
-        )}
       </Suspense>
     </Container>
   )
